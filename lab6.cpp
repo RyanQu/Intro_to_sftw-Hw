69e78907ea1e35e01de1de03531bdcb5f1fa77f9
RyanQu <ryanqu94@sina.com>
2017-11-01 15:58:55 -0400

    lab 6

---
+++ lab6.cpp
@@ -0,0 +1,47 @@
+// labwork 6
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverse_vector(vector<int> &x){
+    cout<<"Reverse the vecotr."<<endl;
+    for(int i=0;i<x.size()/2;i++) {
+        int temp=x[i];
+        x[i]=x[x.size()-1-i];
+        x[x.size()-1-i]=temp;
+    }
+}
+
+void swap_vector(vector<int> &x){
+    cout<<"Reverse the vecotr by swap."<<endl;
+    for(int i=0;i<x.size()/2;i++) {
+        swap(x[i],x[x.size()-1-i]);
+    }
+}
+
+void print_vector(vector<int> &x){
+    cout<<"The vecotr is: { ";
+    for(int i=0;i<x.size()-1;i++) {
+        cout << x[i] <<", ";
+    }
+    cout<<x[x.size()-1]<<" }"<<endl;
+}
+
+int main(){
+    vector<int> nums;
+    int num;
+    cout<<"please input integer: ";
+    while (cin >> num) {
+        nums.push_back(num);
+        cout << "Pushing " << num << " into vector nums."<< endl;
+        cout<<"please input integer: ";
+    }
+    cout<<"\n";
+    print_vector(nums);
+    reverse_vector(nums);
+    print_vector(nums);
+    swap_vector(nums);
+    print_vector(nums);
+    return 0;
+}
+
